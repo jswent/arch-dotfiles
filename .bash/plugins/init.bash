5543aa32e618a4e3f1431260b11786df71ff93f5
James Swent <jake@osprey21.com>
2022-06-29 14:34:04 -0400

    added minimal bash configuration

---
+++ .bash/plugins/init.bash
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+export GIT_PS1_SHOWDIRTYSTATE=1 
+export GIT_PS1_SHOWSTASHSTATE=1 
+export GIT_PS1_SHOWUNTRACKEDFILES=1 
+
+source ~/.bash/plugins/git-completion.bash 
+source ~/.bash/plugins/git-prompt.sh 
+
+source ~/.bash/plugins/completion.bash 
+source ~/.bash/plugins/key-bindings.bash
